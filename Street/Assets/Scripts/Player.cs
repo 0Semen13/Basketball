ca10f93f32fe4d9df8c356b8e1331a2247fd3031
Semen <semenmoiseev.2004@mail.ru>
2023-07-05 14:21:22 +0700

    Cleaning
    Почистил код игрока, уменьшил его на 50 строк

--- Street/Assets/Scripts/Player.cs
+++ Street/Assets/Scripts/Player.cs
@@ -10,32 +10,6 @@ public class Player : MonoBehaviour {
     [SerializeField] private bool isPhone;
 
     [SerializeField] private float speed; //Скорость
-
-    [SerializeField] private Transform player; //Игрок
-    [SerializeField] private Transform ball; //Мяч
-    [SerializeField] private Transform hands; //Руки
-    [SerializeField] private Transform rightHand; //Праввая рука
-    [SerializeField] private Transform posOverHead; //Позиция рук над головой
-    [SerializeField] private Transform posDribble; //Позиция рук внизу
-    [SerializeField] private Transform target; //Цель
-    [SerializeField] private Transform miss1; //Промахи
-    [SerializeField] private Transform miss2;
-    [SerializeField] private Transform miss3;
-    [SerializeField] private Transform miss4;
-    [SerializeField] private Transform miss5;
-    [SerializeField] private Transform miss6;
-    [SerializeField] private Transform teleportPosition; //Позиция телепорта
-    [SerializeField] private Transform PositionBall;
-
-    [SerializeField] private Transform[] Misses; //Массив точек промаха
-
-    private bool ballInHands = false; //Мяч в руках
-    private bool ballFlying = false; //Мяч летит (Не в руках)
-    private bool ballStart = true; //Мяч при старте
-    private bool Ban = false; //Запрет на бросок
-    private bool Ban2 = false; //Запрет на бросок 2
-    private float t0 = 0;
-
     [SerializeField] private double currentPercentage2Point = 70; //Текущие значения процентов
     [SerializeField] private double currentPercentage3Points = 60;
     [SerializeField] private double currentPercentageExtraLong = 50;
@@ -44,12 +18,41 @@ public class Player : MonoBehaviour {
     [SerializeField] private double startPercentageExtraLong = 50;
     private float chanceForBar;
 
+    [SerializeField] private Transform player; //Игрок
+    [SerializeField] private Transform ball; //Мяч
+    [SerializeField] private Transform hands; //Руки
+    [SerializeField] private Transform rightHand; //Правая рука
+    [SerializeField] private Transform posOverHead; //Позиция рук над головой
+    [SerializeField] private Transform posDribble; //Позиция рук внизу
+    [SerializeField] private Transform target; //Цель
+    [SerializeField] private Transform[] Misses; //Массив точек промаха
+    [SerializeField] private Transform miss1; //Промахи
+    [SerializeField] private Transform miss2;
+    [SerializeField] private Transform miss3;
+    [SerializeField] private Transform miss4;
+    [SerializeField] private Transform miss5;
+    [SerializeField] private Transform miss6;
+    [SerializeField] private Transform teleportPosition; //Позиция телепорта
+    [SerializeField] private Transform PositionBall;
+    [SerializeField] private GameObject canvasControlPhone;
+    [SerializeField] private GameObject GOBar;
+
+    private bool ballInHands = false; //Мяч в руках
+    private bool ballFlying = false; //Мяч летит (Не в руках)
+    private bool ballStart = true; //Мяч при старте
+    private bool Ban = false; //Запрет на бросок за пределами площадки
+    private bool Ban2 = false; //Запрет на бросок из мертвой зоны
+    private float t0 = 0;
+
     private bool TwoPoint = false;
     private bool ThreePoint = false;
     private bool SuperPoint = true;
     private bool isHit = false;
 
-    private int num = 0; //Число, сравнивоемое с вероятностью
+    public bool ButtonUpB = false;
+    public bool ButtonDownB = false;
+
+    private int num = 0; //Число, сравниваемое с вероятностью
     private int pnt = 0; //Число, сколько очков должно добавиться после попадания
     private int mss = 0; //Число, означающее индекс промаха при промахе
 
@@ -59,15 +62,9 @@ public class Player : MonoBehaviour {
     [SerializeField] private Text pointDisplay;
     [SerializeField] private Text ballDisplay;
 
-    [SerializeField] private SaveAndLoad SaveScript; //Объект со скриптом сохранения и загрузки
-
     [SerializeField] private Joystick joystick;
 
-    public bool ButtonUpB = false;
-    public bool ButtonDownB = false;
-
-    [SerializeField] private GameObject canvasControlPhone;
-    [SerializeField] private GameObject GOBar;
+    [SerializeField] private SaveAndLoad SaveScript; //Объект со скриптом сохранения и загрузки
 
     private void Start() {
         GOBar.gameObject.SetActive(false);
@@ -94,122 +91,76 @@ public class Player : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        Vector3 direction = new Vector3(0, 0, 0);
         if (isPC) {
-            Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            transform.position += direction * speed * Time.deltaTime; //Движение игрока
-            transform.LookAt(transform.position + direction); //Поворот игрока
-
-            if (ballInHands) {
-                isHit = false;
-                ballStart = false;
-                pnt = 0;
-
-                if (Input.GetKey(KeyCode.Space) && !Ban && !Ban2) {
-                    GOBar.gameObject.SetActive(true);
-                    ball.position = posOverHead.position; //Поднятие рук и мяча при зажатом пробеле и мяче в руках
-                    rightHand.localEulerAngles = Vector3.left * 0;
-                    hands.localEulerAngles = Vector3.right * 180;
-
-                    transform.LookAt(target.position); //Поворот к кольцу
-                    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-                }
-                else {
-                    ball.position = posDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5) * 2);
-                    hands.localEulerAngles = Vector3.right * 0;
-                    rightHand.localEulerAngles = Vector3.left * 50;
-                }
-
-                if (Input.GetKeyUp(KeyCode.Space) && !Ban && !Ban2 && !ballFlying) {
-                    rightHand.localEulerAngles = Vector3.left * 0;
-                    t0 = 0;
-                    num = Random.Range(1, 101); //Определяет число, для сравнения с вероятностью
-                    mss = Random.Range(1, 7); //Определяет, куда попадет промах
-
-                    chanceForBar = GameObject.Find("Chance_Bar").GetComponent<Bar>().chance;
-                    if (chanceForBar > 100 && chanceForBar <= 105) {
-                        chanceForBar = 110;
-                    }
-                    else if (chanceForBar > 105) {
-                        chanceForBar = 35;
-                    }
-
-                    Debug.Log("Число: " + num);
-                    Debug.Log("Шанс шкалы: " + chanceForBar);
-
-                    ballInHands = false;
-                    ballFlying = true;
-                }
-            }
-
-            if (ballFlying && !ballStart) {
-                GameObject.Find("Chance_Bar").GetComponent<Bar>().chance = chanceForBar;
-                ThrowFunction();
-            }
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         }
 
         if (isPhone) {
-            Vector3 direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-            transform.position += direction * speed * Time.deltaTime; //Движение игрока
-            transform.LookAt(transform.position + direction); //Поворот игрока
+            direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        }
 
-            if (ballInHands) {
-                isHit = false;
-                ballStart = false;
-                pnt = 0;
+        transform.position += direction * speed * Time.deltaTime; //Движение игрока
+        transform.LookAt(transform.position + direction); //Поворот игрока
 
-                if (ButtonDownB && !Ban && !Ban2) {
-                    GOBar.gameObject.SetActive(true);
-                    ball.position = posOverHead.position; //Поднятие рук и мяча при зажатом пробеле и мяче в руках
-                    rightHand.localEulerAngles = Vector3.left * 0;
-                    hands.localEulerAngles = Vector3.right * 180;
+        if (ballInHands) {
+            isHit = false;
+            ballStart = false;
+            pnt = 0;
 
-                    transform.LookAt(target.position);
-                    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-                }
-                else {
-                    ball.position = posDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5) * 2);
-                    hands.localEulerAngles = Vector3.right * 0;
-                    rightHand.localEulerAngles = Vector3.left * 50;
-                    ButtonDownB = false;
-                }
+            if ((ButtonDownB || Input.GetKey(KeyCode.Space)) && !Ban && !Ban2) {
+                GOBar.gameObject.SetActive(true);
+                ball.position = posOverHead.position; //Поднятие рук и мяча при зажатом пробеле и мяче в руках
+                rightHand.localEulerAngles = Vector3.left * 0;
+                hands.localEulerAngles = Vector3.right * 180;
 
-                if (ButtonUpB && !Ban && !Ban2 && !ballFlying) {
-                    hands.localEulerAngles = Vector3.right * 0;
-                    rightHand.localEulerAngles = Vector3.left * 0;
-
-                    t0 = 0;
-                    num = Random.Range(1, 101); //Определяет число, для сравнения с вероятностью
-                    mss = Random.Range(1, 7); //Определяет, куда попадет промах
-
-                    chanceForBar = GameObject.Find("Chance_Bar").GetComponent<Bar>().chance;
-                    if (chanceForBar > 100 && chanceForBar <= 105) {
-                        chanceForBar = 110;
-                    }
-                    else if (chanceForBar > 105) {
-                        chanceForBar = 35;
-                    }
-
-                    Debug.Log("Число: " + num);
-                    Debug.Log("Шанс шкалы: " + chanceForBar);
-
-                    ButtonDownB = false;
-                    ballInHands = false;
-                    ballFlying = true;
-                    ButtonUpB = false;
-                }
-                else {
-                    ButtonUpB = false;
-                }
+                transform.LookAt(target.position);
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
             }
             else {
+                ball.position = posDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5) * 2);
+                hands.localEulerAngles = Vector3.right * 0;
+                rightHand.localEulerAngles = Vector3.left * 50;
+
                 ButtonDownB = false;
-                ButtonUpB = false;
             }
 
-            if (ballFlying && !ballStart) {
-                GameObject.Find("Chance_Bar").GetComponent<Bar>().chance = chanceForBar;
-                ThrowFunction();
+            if ((ButtonUpB || Input.GetKeyUp(KeyCode.Space)) && !Ban && !Ban2 && !ballFlying) {
+                hands.localEulerAngles = Vector3.right * 0;
+                rightHand.localEulerAngles = Vector3.left * 0;
+
+                t0 = 0;
+                num = Random.Range(1, 101); //Определяет число, для сравнения с вероятностью
+                mss = Random.Range(1, 7); //Определяет, куда попадет промах
+
+                chanceForBar = GameObject.Find("Chance_Bar").GetComponent<Bar>().chance;
+                if (chanceForBar > 100 && chanceForBar <= 105) {
+                    chanceForBar = 110;
+                }
+                else if (chanceForBar > 105) {
+                    chanceForBar = 35;
+                }
+
+                Debug.Log("Число: " + num);
+                Debug.Log("Шанс шкалы: " + chanceForBar);
+
+                ButtonDownB = false;
+                ballInHands = false;
+                ballFlying = true;
+                ButtonUpB = false;
             }
+            else {
+                ButtonUpB = false;
+            }
+        }
+        else {
+            ButtonDownB = false;
+            ButtonUpB = false;
+        }
+
+        if (ballFlying && !ballStart) {
+            GameObject.Find("Chance_Bar").GetComponent<Bar>().chance = chanceForBar;
+            ThrowFunction();
         }
 
         if (developerMode) {                    //ДЛЯ РАЗРАБОТЧИКА
